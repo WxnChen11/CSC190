2437d15d58f4f0ce0229d908db67c92c09ab204d
wxchen11 <wenxinchen11@hotmail.com>
2016-02-18 01:34:16 -0500

    lab4 added

---
+++ lab4/code/vInterface.c
@@ -0,0 +1,14 @@
+#include "lab4.h"
+
+struct vInterface initVInterface(int (*sS)(int), int (*sG)(int), int (*sP)(int))
+{
+
+vInterface interface;
+interface.setSpeed = sS;
+interface.setGear = sG;
+interface.setPassengers = sP;
+
+return interface;
+
+
+}
